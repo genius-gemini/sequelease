056be933eb4f9e1aa9bc3b8c75a82c2a9eca26fb
kylebirns <kylebirns@gmail.com>
2019-03-13 13:05:02 -0400

    Bug: Replaced db.js with boilermakers

--- server/db/db.js
+++ server/db/db.js
@@ -1,23 +1,41 @@
-const pkg = require('../../package.json'); // For db name
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
 
-const Sequelize = require('sequelize');
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const dbName = pkg.name;
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
+console.log('Seeding database: ', databaseName)
+module.exports = db
 
-const dbOptions = process.env.DATABASE_OPTIONS || {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: dbName,
-};
-dbOptions.logging = false;
-
-const db = new Sequelize(dbOptions);
-
-module.exports = db;
-
-// This is a global Mocha hook used for resource cleanup.
-// Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => db.close());
+  after('close database connection', () => db.close())
 }
+
+// const pkg = require('../../package.json'); // For db name
+
+// const Sequelize = require('sequelize');
+
+// const dbName = pkg.name;
+
+// const dbOptions = process.env.DATABASE_OPTIONS || {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   database: dbName,
+// };
+// dbOptions.logging = false;
+
+// const db = new Sequelize(dbOptions);
+
+// module.exports = db;
+
+// // This is a global Mocha hook used for resource cleanup.
+// // Otherwise, Mocha v4+ does not exit after tests.
+// if (process.env.NODE_ENV === 'test') {
+//   after('close database connection', () => db.close());
+// }
